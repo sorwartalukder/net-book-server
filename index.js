161d87f26b917bd80f7b2cc065d01fd72818388c
sorwartalukder <mdsorwar4039@gmail.com>
2022-12-28 22:44:23 +0600

    send complete notes and delete notes

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     const userCollection = client.db('netBook').collection('users')
     const noteCollection = client.db('netBook').collection('notes')
+
+    //send notes client
+    app.get('/notes/:email', async (req, res) => {
+        const email = req.params.email
+        const query = { email }
+        const allNote = await noteCollection.find(query, { sort: { _id: -1 } }).toArray()
+        const notes = allNote.filter(n => !n.completed)
+        res.send(notes);
+    })
+    //send completed notes client
+    app.get('/completed-notes/:email', async (req, res) => {
+        const email = req.params.email
+        const query = { email }
+        const allNote = await noteCollection.find(query, { sort: { _id: -1 } }).toArray()
+        const notes = allNote.filter(n => n.completed)
+        res.send(notes);
+    })
     // add user database 
     app.post('/users', async (req, res) => {
         const newUser = req.body;
@@ -30,6 +47,24 @@ async function run() {
         const note = req.body;
         const result = await noteCollection.insertOne(note)
         res.send(result)
+    })
+    // update completed situation
+    app.put('/notes/:id', async (req, res) => {
+        const id = req.params.id;
+        const complete = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: complete
+        }
+        const result = await noteCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+    })
+    app.delete('/notes/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await noteCollection.deleteOne(query);
+        res.send(result);
     })
 }
 
